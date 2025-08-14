a845034d373cd8e20df21b092c04bfe701bb0451
himani2815 <himanibhardwaj411411@gmail.com>
2025-08-14 11:08:21 +0530

    lab  q -1 assingment 3

---
+++ emp.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleApp4;
+using System;
+namespace ConsoleApp4
+{
+    class employee
+    {
+        public String name = "hina";
+        public int  age = 22;
+        public double salary = 170000;
+      /
+        public void display()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Salary: " + salary);
+
+
+        }
+
+    }
+}
+ class Program
+{
+    static void Main(String[] args)
+    {
+        employee emp = new employee();
+        emp.display();
+    }
+}
+    
+      
+
